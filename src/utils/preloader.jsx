4a1255fd965ef2f5c4c82f724d773edfaba59fb2
Ken Wheeler <ken_wheeler@me.com>
2015-06-30 18:31:59 -0400

    GH Pages

--- src/utils/preloader.jsx
+++
@@ -1,7 +0,0 @@
-export default function preload(imageArray) {
-  var images = new Array()
-  for (var i = 0; i < imageArray.length; i++) {
-    images[i] = new Image()
-    images[i].src = imageArray[i]
-  }
-}
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var getConfig = require('hjs-webpack')
-var config = require('./presentation/config');
-
-module.exports = getConfig({
-  in: './index.jsx',
-  out: 'dist',
-  clearBeforeBuild: true,
-  html: config.html
-});
